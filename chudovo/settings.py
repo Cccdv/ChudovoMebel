1ea4a9ef129d5bfff1b36e973082b1fb32fb9960
zyxel09 <zyxel09@mail.ru>
2022-06-03 01:29:39 +0300

    make it better

--- chudovo/settings.py
+++ chudovo/settings.py
@@ -59,8 +59,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -179,14 +180,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
