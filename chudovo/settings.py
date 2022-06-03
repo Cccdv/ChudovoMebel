f23dcccfd50feca58de40de4c88f1fe8cea0a983
zyxel09 <zyxel09@mail.ru>
2022-06-03 13:21:14 +0300

    added migration for app someapp

--- chudovo/settings.py
+++ chudovo/settings.py
@@ -134,16 +134,17 @@ WSGI_APPLICATION = 'chudovo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd227tiit39o0m1',
-        'USER': 'egtvdovazlztew',
-        'PASSWORD': 'b3915e828363fe61718605fb9b8281d232bb22f977dcbaa36c348e761793dd5d',
-        'HOST': 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -207,14 +208,14 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'chudovomebel@gmail.com'
 else:
 
-    DEFAULT_FROM_EMAIL = 'admin@mail.com'
-    SERVER_EMAIL = 'admin@mail.com'
+    DEFAULT_FROM_EMAIL = 'mebelchudovo@yandex.ru'
+    SERVER_EMAIL = 'mebelchudovo@yandex.ru'
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'chudovomebel@gmail.com'
-    EMAIL_HOST_PASSWORD = 'Pub1ch3r0'
-    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.yandex.ru'
+    EMAIL_HOST_USER = 'mebelchudovo@yandex.ru'
+    EMAIL_HOST_PASSWORD = 'H4rdsk1lljusttruebr0'
+    EMAIL_PORT = 465
 
 if 'USE_AWS' in os.environ:
     # # Cache control
