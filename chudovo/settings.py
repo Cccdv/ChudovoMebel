2c046947df108fdc464c6a7c9dc55c52bf84ec3e
zyxel09 <zyxel09@mail.ru>
2022-06-05 00:11:02 +0300

    Added custom 404 and 500 error pages.

--- chudovo/settings.py
+++ chudovo/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '*')
 
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #work with True ONLY
